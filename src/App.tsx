2052e561e9150a093ac7b4ba66d0c306a5aef45c
guillenec <guillermoneculqueo@gmail.com>
2025-03-21 23:53:10 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <body className='mx-auto flex justify-center items-center h-screen w-screen '>
+        <h1 className='text-3xl font-bold underline'>KutralBeer</h1>
+      </body>
+    </>
+  )
+}
+
+export default App
