c8477d8e642468dfa4b7b3e0fe71080997717502
guillenec <guillermoneculqueo@gmail.com>
2025-03-22 00:11:06 -0300

    feat: agregar navegación y título principal

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import './App.css'
 
 function App() {
 
+
+  const navHover = 'hover:text-amber-300 hover:ease-in duration-200'
+  
   return (
     <>
-      <body className='mx-auto flex justify-center items-center h-screen w-screen '>
-        <h1 className='text-3xl font-bold underline'>KutralBeer</h1>
+      <body className='flex flex-col h-screen w-screen justify-start items-center'>
+        <header className='flex justify-between w-full p-4 bg-slate-900 text-white'>
+          <nav className='flex'>
+            <a href="#" className={`mr-4 ${navHover}`}>Home</a>
+            <a href="#" className={`mr-4 ${navHover}`}>About</a>
+            <a href="#" className={`mr-4 ${navHover}`}>Contact</a>
+          </nav>
+        </header>
+
+        <main className='flex w-full h-full flex-col items-center justify-center'>
+          <h1 className='text-3xl font-bold'>KutralBeer</h1>
+        </main>
       </body>
     </>
   )
